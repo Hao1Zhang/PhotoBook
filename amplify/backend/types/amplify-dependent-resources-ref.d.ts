20ec9f1daac85e7765be92fc70273044dc5acbf7
Hao Zhang <zhangh0904_tj@126.com>
2022-01-17 21:30:22 +1030

    login backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "photobook1e1b1ff8": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
---
+++ src/components/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+export default {
+    data:()=>({
+        username:"",
+        password:"",
+        email:"",
+        error:""
+    }),
+    method:{
+        ...mapActions({
+            loginVue: "auth/login",
+        }),
+        async login(){
+            try {
+                await this.loginVue({
+                    username: this.username,
+                    password: this.password,
+                });
+            } catch (e) {
+                this.error = e.message;
+            }
+
+        }
+    },
+};
+</script>
+
+<style></style>
