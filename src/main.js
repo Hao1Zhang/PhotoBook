e1117bab6699aec8698d0d8a33fdf5d111ee0e19
Hao Zhang <zhangh0904_tj@126.com>
2022-01-17 19:12:37 +1030

    added amplify backend

--- src/main.js
+++ src/main.js
@@ -2,9 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/tailwind.css'
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
 
 Vue.config.productionTip = false
 
+
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@aws-amplify/ui-components/loader';
+
+Amplify.configure(aws_exports);
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
+
+Vue.config.ignoredElements = [/amplify-\w*/];
 new Vue({
   router,
   store,
